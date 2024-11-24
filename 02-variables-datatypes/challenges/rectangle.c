064d8935c5574b948c94b19a48b849cc68a3efae
Chen Liang <chen.liang.cn@outlook.com>
2024-11-24 17:55:11 +0800

    command line arguments

--- 02-variables-datatypes/challenges/rectangle.c
+++ 02-variables-datatypes/challenges/rectangle.c
@@ -4,7 +4,7 @@ int main()
 {
     double width = 1.0;
     double height = 2.0;
-    double perimeter = (width + height) * 2;
+    double perimeter = (width + height) * 2.0;
     double area = width * height;
 
     printf("Rectangle width: %.2lf\n", width);
