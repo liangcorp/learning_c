aa7deb273df453d9dd4801641fdf2256123d3325
Chen Liang <chen.liang.cn@outlook.com>
2024-11-24 19:50:34 +0800

    relational operators

--- 03-operators/assignment_operators.c
+++ 03-operators/assignment_operators.c
@@ -41,14 +41,5 @@ int main()
     a |= 2;
     printf("a |= 2; a = %d\n", a);
 
-    printf("a = %d\n", a);
-    printf("b = %d\n", b);
-    printf("a == b: %d\n", a == b);
-    printf("a != b: %d\n", a != b);
-    printf("a > b: %d\n", a > b);
-    printf("a < b: %d\n", a < b);
-    printf("a >= b: %d\n", a >= b);
-    printf("a <= b: %d\n", a <= b);
-
     return 0;
 }
---
+++ 03-operators/relational_operators.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 0;
+    int b = 1;
+
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+    printf("a == b: %d\n", a == b);
+    printf("a != b: %d\n", a != b);
+    printf("a > b: %d\n", a > b);
+    printf("a < b: %d\n", a < b);
+    printf("a >= b: %d\n", a >= b);
+    printf("a <= b: %d\n", a <= b);
+
+    return 0;
+}
