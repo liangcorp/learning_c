efcc79e5dc33f50247ac5d95cc2791dd1e368bb2
Chen Liang <chen.liang.cn@outlook.com>
2024-11-24 19:26:50 +0800

    basic operations

---
+++ 03-operators/basic_operators.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 0;
+    int b = 1;
+
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+    printf("a + b = %d\n", a + b);
+    printf("a - b = %d\n", a - b);
+    printf("a * b = %d\n", a * b);
+    printf("a / b = %d\n", a / b);
+    printf("a %% b = %d\n", a % b);
+    printf("a++ = %d\n", a++);
+    printf("++a = %d\n", ++a);
+    printf("a-- = %d\n", a--);
+    printf("--a = %d\n", --a);
+
+    return 0;
+}
