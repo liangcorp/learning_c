d362138c123b897563387cf60f6933fdbc729f3a
Chen Liang <chen.liang.cn@outlook.com>
2024-11-25 21:39:26 +0800

    continue

---
+++ 04-control-flow/continue.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+
+    enum Day {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    };
+
+    for (enum Day day = Monday; day <= Sunday; day++)
+        continue;
+
+    printf("It's not Wednesday!\n");
+
+    return 0;
+}
