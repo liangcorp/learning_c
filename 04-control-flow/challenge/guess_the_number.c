1bf6c601e83cfcfad3298d640dd815a24ebb9106
Chen Liang <chen.liang.cn@outlook.com>
2024-11-25 22:58:00 +0800

    add checks for invalid numbers

--- 04-control-flow/challenge/guess_the_number.c
+++ 04-control-flow/challenge/guess_the_number.c
@@ -6,22 +6,24 @@
 
 int main()
 {
-    time_t t;
-    srand((unsigned) time(&t));
+	time_t t;
+	srand((unsigned)time(&t));
 
 	int i;
 	int input = 0;
 	int random = rand() % 21;
 
-
 	for (i = 0; i < MAX_NO_GUESSES; i++) {
-		printf("You have %d guess%s left.\n", 5 - i, (5 - i) == 1 ? "" : "es" );
+		printf("You have %d guess%s left.\n", 5 - i,
+		       (5 - i) == 1 ? "" : "es");
 		printf("Enter a guess: ");
 		scanf("%d", &input);
 
 		if (input == random) {
 			printf("Congraulations. You guessed it!\n");
 			break;
+		} else if (input < 0 || input > 20) {
+			printf(" I said the number is between 0 and 20\n");
 		} else if (input < random) {
 			printf("Sorry, %d is wrong. My number is higher than that.\n\n",
 			       input);
