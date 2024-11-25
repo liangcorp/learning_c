6c1b16341a8db4502a742500c346e32015808a84
Chen Liang <chen.liang.cn@outlook.com>
2024-11-25 21:54:36 +0800

    guess the number

---
+++ 04-control-flow/challenge/guess_the_number.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    time_t t;
+    srand((unsigned) time(&t));
+
+	int i;
+	int input = 0;
+	int random = rand() % 21;
+
+
+	for (i = 0; i < 5; i++) {
+		printf("You have %d guest left.\n", 5 - i);
+		printf("Enter a guess: ");
+		scanf("%d", &input);
+
+		if (input == random) {
+			printf("Congraulations. You guessed it!\n");
+			break;
+		} else if (input < random) {
+			printf("Sorry, %d is wrong. My number is higher than that.\n\n",
+			       input);
+		} else {
+			printf("Sorry, %d is wrong. My number is less than that.\n\n",
+			       input);
+		}
+	}
+
+	return 0;
+}
