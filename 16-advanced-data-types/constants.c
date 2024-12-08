c805c557d43dbbc8738440f606c28c8401484525
Chen Liang <chen.liang.cn@outlook.com>
2024-12-08 12:10:48 +0800

    constants

---
+++ 16-advanced-data-types/constants.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#define TWO_PI 2.0 * 3.141592654
+
+#define PI 3.141592654
+
+#define YES 1
+
+#define NULL 0
+
+const float Pi = 3.141592654f;  // Defines the value of Pi as fixed
+
+/*
+    #define is replacing all occurrence of PI in your code
+    const is defining Pi as a constant numerical value with a type
+*/
+
+int main()
+{
+    printf("%f\n", TWO_PI);
+
+    return NULL;
+}
